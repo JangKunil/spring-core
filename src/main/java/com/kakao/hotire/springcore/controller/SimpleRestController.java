5409d72e79fa5516286db63aeab485af343f3068
JangKunil <jangkeya@gmail.com>
2023-05-26 00:58:36 +0900

    Remove duplicated comment

--- src/main/java/com/kakao/hotire/springcore/controller/SimpleRestController.java
+++ src/main/java/com/kakao/hotire/springcore/controller/SimpleRestController.java
@@ -1,14 +1,24 @@
 package com.kakao.hotire.springcore.controller;
 
+import com.kakao.hotire.springcore.bean.Proto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
 
+  @Autowired(required = false)
+  private Proto proto;
+
   @GetMapping("/hello3")
   public String hello() {
     return "hello3";
+  }
+
+  @GetMapping("/hello4")
+  public String h() {
+    return proto.toString();
   }
 
 }
