8d0488550822a8e1b2d8cbf3a0c4a6204cb01843
jangk <jangkeya@gmail.com>
2023-06-23 21:21:34 +0900

    add filter

--- src/main/java/com/kakao/hotire/springcore/config/OnMissingProfile.java
+++ src/main/java/com/kakao/hotire/springcore/config/OnMissingProfile.java
@@ -17,10 +17,17 @@ public class OnMissingProfile implements Condition {
 
         return Optional.ofNullable(attrs)
                        .map(it -> (List<?>) it.get("value"))
+                       .map(it -> {
+                           System.out.println(it);
+                           return it;
+                       })
                        .map(profiles -> profiles.stream()
                                                 .map(Object::toString)
                                                 .map(Profiles::of)
-                                                .noneMatch(profile -> context.getEnvironment().acceptsProfiles(profile)))
+                                                .filter(profile -> context.getEnvironment().acceptsProfiles(profile))
+                                                .findAny()
+                                                .map(it -> Boolean.FALSE)
+                                                .orElse(true))
                        .orElse(true);
     }
 }
